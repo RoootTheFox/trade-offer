83ab7e7b7e653061a689ca44fd56ede58d8d04bf
Lucy <snowisacatgirl@duck.com>
2025-01-01 10:49:07 +0100

    refactor: less magic values, and FINAL. (#1)
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main/java/gay/rooot/tradeoffer/TradeOfferMeow.java
+++ src/main/java/gay/rooot/tradeoffer/TradeOfferMeow.java
@@ -6,33 +6,54 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
 public class TradeOfferMeow {
-    public static MutableText TRADE_OFFER = Text.translatable("TRADE OFFER");
+    public static final MutableText TRADE_OFFER_TEXT = Text.literal("TRADE OFFER");
+    private static final Text I_RECEIVE_TEXT = Text.of("i receive:");
+    private static final Text YOU_RECEIVE_TEXT = Text.of("you receive:");
+    private static final Text TRIANGLE_TEXT = Text.of("⚠");
 
-    // dedicated method for hot reloading meow
-    public static void hotDemonTwinksInHell(DrawContext context, TextRenderer textRenderer, int backgroundWidth) {
-        var i_get_text = Text.of("i receive:");
-        context.drawText(textRenderer, i_get_text, 17 + backgroundWidth / 2 - textRenderer.getWidth(i_get_text) / 2, 20, 0xffffff, true);
+    private static final int I_RECEIVE_X = 17;
+    private static final int YOU_RECEIVE_X = 89;
+    private static final int TRADE_OFFER_X = 49;
 
-        var u_get_text = Text.of("you receive:");
-        context.drawText(textRenderer, u_get_text, 89 + backgroundWidth / 2 - textRenderer.getWidth(u_get_text) / 2, 20, 0xffffff, true);
+    private static final int TEXT_Y = 20;
+    private static final int TRIANGLE_Y = 6;
 
-        // draw stupid ⚠⚠⚠⚠⚠⚠
-        var trade_offer_w = textRenderer.getWidth(TRADE_OFFER);
-        var trade_offer_pos = 49 + backgroundWidth / 2 - trade_offer_w / 2;
+    private static final int TRIANGLE_PADDING = 4;
+    private static final int BACKGROUND_PADDING = 3;
 
-        var triangle_dist = 4;
-        var bg_thingy = 3;
+    private static final int BACKGROUND_COLOR = 0xffe13d3b;
+    private static final int TEXT_COLOR = 0xffffff;
+    private static final int TRIANGLE_COLOR = 0xffff00;
 
-        var triangle = Text.of("⚠");
-        var triangle_w = textRenderer.getWidth(triangle);
+    public static void hotDemonTwinksInHell(final DrawContext ctx, final TextRenderer renderer, final int backgroundWidth) {
+        int tradeOfferWidth = renderer.getWidth(TRADE_OFFER_TEXT);
+        int tradeOfferX = TRADE_OFFER_X + (backgroundWidth / 2) - (tradeOfferWidth / 2);
+        int triangleWidth = renderer.getWidth(TRIANGLE_TEXT);
+
+        drawCenterText(ctx, renderer, I_RECEIVE_TEXT, I_RECEIVE_X, TEXT_Y, backgroundWidth, TEXT_COLOR);
+        drawCenterText(ctx, renderer, YOU_RECEIVE_TEXT, YOU_RECEIVE_X, TEXT_Y, backgroundWidth, TEXT_COLOR);
 
         // no. i am not making it rounded, fuck you. go pr if you want idrc
-        context.fill(trade_offer_pos - triangle_w - triangle_dist - bg_thingy,
-                6 - bg_thingy,
-                trade_offer_pos + trade_offer_w + triangle_w + triangle_dist + bg_thingy,
-                16, 0xffe13d3b);
+        ctx.fill(tradeOfferX - triangleWidth - BACKGROUND_PADDING - TRIANGLE_PADDING,
+                6 - BACKGROUND_PADDING,
+                tradeOfferX + tradeOfferWidth + triangleWidth + BACKGROUND_PADDING + TRIANGLE_PADDING,
+                16,
+                BACKGROUND_COLOR
+        );
 
-        context.drawText(textRenderer, triangle, trade_offer_pos - (triangle_w + triangle_dist), 6, 0xffff00, false);
-        context.drawText(textRenderer, triangle, trade_offer_pos + triangle_dist + trade_offer_w, 6, 0xffff00, false);
+        // draw stupid ⚠⚠⚠⚠⚠⚠
+        ctx.drawText(renderer, TRIANGLE_TEXT, tradeOfferX - triangleWidth - TRIANGLE_PADDING, TRIANGLE_Y, TRIANGLE_COLOR, false);
+        ctx.drawText(renderer, TRIANGLE_TEXT, tradeOfferX + tradeOfferWidth + TRIANGLE_PADDING + 1, TRIANGLE_Y, TRIANGLE_COLOR, false);
+    }
+
+    @SuppressWarnings("SameParameterValue") // yOffset and color gives a warning which we can just...suppress because it's a helper function
+    private static void drawCenterText(DrawContext ctx, TextRenderer renderer, Text text, int xOffset, int yOffset, int backgroundWidth, int color) {
+        ctx.drawTextWithShadow(
+                renderer,
+                text,
+                xOffset + (backgroundWidth / 2) - (renderer.getWidth(text) / 2),
+                yOffset,
+                color
+        );
     }
 }
--- src/main/java/gay/rooot/tradeoffer/client/mixins/MerchantScreenMixin.java
+++ src/main/java/gay/rooot/tradeoffer/client/mixins/MerchantScreenMixin.java
@@ -4,6 +4,7 @@ import gay.rooot.tradeoffer.TradeOfferMeow;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.MerchantScreenHandler;
 import net.minecraft.text.MutableText;
@@ -16,19 +17,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static gay.rooot.tradeoffer.TradeOfferMeow.TRADE_OFFER;
+import static gay.rooot.tradeoffer.TradeOfferMeow.TRADE_OFFER_TEXT;
 
-@Mixin(net.minecraft.client.gui.screen.ingame.MerchantScreen.class)
-public abstract class VillagerUIMixin extends HandledScreen<MerchantScreenHandler> {
+@Mixin(MerchantScreen.class)
+public abstract class MerchantScreenMixin extends HandledScreen<MerchantScreenHandler> {
+
     @Unique
-    boolean isTradingRightMeow = false;
+    private boolean isTradingRightMeow = false;
 
-    public VillagerUIMixin(MerchantScreenHandler handler, PlayerInventory inventory, Text title) {
+    public MerchantScreenMixin(MerchantScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
     }
 
     @Inject(at = @At("HEAD"), method = "render")
-    public void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    public void render(final DrawContext context, final int mouseX, final int mouseY, final float delta, final CallbackInfo ci) {
         if (this.handler.slots.size() < 2) {
             // what the f-
             isTradingRightMeow = false;
@@ -40,27 +42,26 @@ public abstract class VillagerUIMixin extends HandledScreen<MerchantScreenHandle
     }
 
     @Inject(at = @At("HEAD"), method = "drawLevelInfo", cancellable = true)
-    public void drawLevelInfo(DrawContext context, int x, int y, TradeOffer tradeOffer, CallbackInfo ci) {
-        if (isTradingRightMeow) {
-            ci.cancel();
-        }
+    public void drawLevelInfo(final DrawContext context, final int x, final int y, final TradeOffer tradeOffer, final CallbackInfo ci) {
+        if (isTradingRightMeow) ci.cancel();
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/text/MutableText;"), method = "drawForeground")
-    public MutableText drawForeground_translatable(String key, Object[] args) {
-        return isTradingRightMeow ? TRADE_OFFER : Text.translatable(key, args);
+    public MutableText drawForeground_translatable(final String key, final Object[] args) {
+        return isTradingRightMeow ? TRADE_OFFER_TEXT : Text.translatable(key, args);
     }
 
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 0), method = "drawForeground")
-    public int drawText_translatable(DrawContext ctx, TextRenderer textRenderer, Text text, int x, int y, int color, boolean shadow) {
+    public int drawText_translatable(final DrawContext ctx, final TextRenderer textRenderer, final Text text, final int x, final int y, int color, final boolean shadow) {
         if (color == 0x404040 && isTradingRightMeow) {
             color = 0xffffff;
         }
+
         return ctx.drawText(textRenderer, text, x, y, color, shadow);
     }
 
     @Inject(at = @At("HEAD"), method = "drawForeground")
-    public void drawForeground(DrawContext context, int mouseX, int mouseY, CallbackInfo ci) {
+    public void drawForeground(final DrawContext context, final int mouseX, final int mouseY, final CallbackInfo ci) {
         if (isTradingRightMeow) {
             TradeOfferMeow.hotDemonTwinksInHell(context, this.textRenderer, this.backgroundWidth);
         }
